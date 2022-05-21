f1921258cb56e12751ae3466dc1d7f3c1c4d834f
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-05-21 23:29:00 +0500

    Completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const phones = document.querySelectorAll(".phones div");
+var phonesArr = Array.from(phones);
+
+for (let i = 0; i < phonesArr.length; i++) {
+  phonesArr[i].addEventListener("mouseover", () => {
+    phonesArr.forEach((elem) => {
+      elem.classList.remove("active");
+      elem.classList.remove("active-2");
+    });
+
+    if (phonesArr.indexOf(phonesArr[i]) === 0) {
+      phonesArr[2].classList.add("active-2");
+    } else if (phonesArr.indexOf(phonesArr[i]) === 2) {
+      phonesArr[0].classList.add("active-2");
+    } else {
+      phonesArr[2].classList.remove("active-2");
+    }
+
+    phonesArr[i].classList.add("active");
+  });
+}
